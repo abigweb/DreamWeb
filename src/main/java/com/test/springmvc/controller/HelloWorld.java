940c638662291d7b0a16b7382ce5f404b188eed9
xucancan <cancan@mywiz.cn>
2018-01-24 15:45:13 +0800

    xucancan

--- src/main/java/com/test/springmvc/controller/HelloWorld.java
+++ src/main/java/com/test/springmvc/controller/HelloWorld.java
@@ -3,12 +3,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/Hello")
+@Controller   //声明为控制器
+@RequestMapping("/Home")  //请求映射
 public class HelloWorld {
-	@RequestMapping("/Sayhi")
+	@RequestMapping("/SayHello")
 	public String SayHi(Model model) {
 		model.addAttribute("message", "Hello Spring MVC!");
-		return "sayhi";
+		return "hello_world";
 	}
 }
---
+++ src/main/webapp/readme.txt
@@ -0,0 +1,8 @@
+在src/main/java/springmvc-servlet.xml文件中的“视图解析器”中说明了
+    WE-INF/view的存放位置
+
+在webapp/WEB-INF/web.xml文件中的“servlet”标签中说明了
+    src/main/java/springmvc-servlet.xml 该配置文件的存放位置
+
+在src/main/java/springmvc-servlet.xml文件中的"自动扫描包" 中说明了
+    控制器（controller）的存放位置在src/main/java/com.test.springmvc.controller下
